5cac5990756ab6c78fdd70192cd7f0ea01d6ab5f
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-04-03 16:20:36 -0300

    feat: route patient get appointments

--- src/controllers/getPatientAppointments.js
+++ src/controllers/getPatientAppointments.js
@@ -1,28 +1,32 @@
 import { db } from "../database/db.js";
 
 export async function getAppointments(req, res) {
-  const patientToken = req.headers.authorization.split(" ")[1];
+    const patientToken = req.headers.authorization.split(" ")[1];
 
-  try {
-    // Query the database to find the patient with the given token
-    const queryPatient = 'SELECT * FROM patient_sessions WHERE token = $1';
-    const valuesPatient = [patientToken];
-    const resultPatient = await db.query(queryPatient, valuesPatient);
+    try {
+        // Query the database to find the patient with the given token
+        const queryPatient = 'SELECT * FROM patient_sessions WHERE token = $1';
+        const valuesPatient = [patientToken];
+        const resultPatient = await db.query(queryPatient, valuesPatient);
 
-    if (resultPatient.rowCount === 0) {
-      return res.status(401).send({ message: 'Invalid token' });
+        if (resultPatient.rowCount === 0) {
+            return res.status(401).send({ message: 'Invalid token' });
+        }
+
+        const patient = resultPatient.rows[0];
+
+        // Query the database to find the appointments of the given patient
+        const queryAppointments = `SELECT appointments.id, doctors.name as doctor_name, available_times.date, available_times.start_time, available_times.end_time, appointments.confirmed, appointments.finished 
+    FROM appointments 
+    JOIN doctors ON doctors.id = appointments.doctor_id
+    JOIN available_times ON available_times.id = appointments.time
+    WHERE appointments.patient_id = $1`;
+        const valuesAppointments = [patient.id];
+        const resultAppointments = await db.query(queryAppointments, valuesAppointments);
+
+        res.status(200).send({ appointments: resultAppointments.rows });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: 'Server error' });
     }
-
-    const patient = resultPatient.rows[0];
-
-    // Query the database to find the appointments of the given patient
-    const queryAppointments = 'SELECT appointments.*, doctors.name as doctor_name, doctors.specialty FROM appointments JOIN doctors ON appointments.doctor_id = doctors.id WHERE patient_id = $1';
-    const valuesAppointments = [patient.id];
-    const resultAppointments = await db.query(queryAppointments, valuesAppointments);
-
-    res.status(200).send({ appointments: resultAppointments.rows });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: 'Server error' });
-  }
 }
--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -3,10 +3,12 @@ import { patientValidation } from "../middlewares/patientSignUp.middleware.js";
 import { patientSignup } from "../controllers/patientSignUp.controller.js";
 import { authenticatePatient } from "../middlewares/patientSignIn.middleware.js";
 import { loginPatient } from "../controllers/patientSignIn.controller.js";
+import { getAppointments } from "../controllers/getPatientAppointments.js";
 
 const patientRoutes = Router();
 
 patientRoutes.post("/signup-patient", patientValidation, patientSignup)
 patientRoutes.post("/signin-patient", authenticatePatient, loginPatient)
+patientRoutes.get("/getPatientAppointments", getAppointments)
 
 export default patientRoutes;
